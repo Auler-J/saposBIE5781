4089cf45bc5db170baf635d0f2ff0fa1c4251d38
Auler-J <auler.jennifer@gmail.com>
2022-11-17 20:46:30 -0300

    mudancas minimas no nome da simulacao

--- R/simula_dados.R
+++ R/simula_dados.R
@@ -1,13 +1,12 @@
 source("R/saposim.R")
 source("R/funcoes_auxiliares.R")
 
-
 # simulacao com valores default: vecarea = round(runif(nsim, 10, 100), 0), vecnmacho= vecarea, cffreq = 1.05, cftama = 0.5, cffoot = 0.8
 # nsim foi colocado como 10000 para ver se o modelo misto converge
-sim1 = saposim(nsim = 10000)
-save(sim1, file = "data/sim1.Rdata")
+sim_Afix = saposim(nsim = 10000)
+save(sim_Afix, file = "data/sim_Afix.Rdata")
 
 #simulacao mais simples, com area constante em 100
-sim2 = saposim(nsim = 1000, vecarea = rep(100, times = 1000))
-save(sim2, file = "data/sim2.Rdata")
-
+# nsim foi colocado como 10000 para ver se o modelo misto para comparacao com simulacao anterior
+sim_Avar = saposim(nsim = 10000, vecarea = rep(100, times = 1000))
+save(sim_Avar, file = "data/sim_Avar.Rdata")
