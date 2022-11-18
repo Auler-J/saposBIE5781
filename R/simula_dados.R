4ccacb9a8032ae4fb540b9368b18a5557b37c8e0
Auler-J <auler.jennifer@gmail.com>
2022-11-18 07:10:28 -0300

    remove simulacao antiga e substitui pela nova (nsim = 10000 e mexia de DF 5700)

--- R/simula_dados.R
+++ R/simula_dados.R
@@ -6,7 +6,17 @@ source("R/funcoes_auxiliares.R")
 sim_Afix = saposim(nsim = 10000)
 save(sim_Afix, file = "data/sim_Afix.Rdata")
 
+#simulacao mais simples, com area constante em 10
+# nsim foi colocado como 10000 para ver se o modelo misto para comparacao com simulacao anterior
+sim_Avar10 = saposim(nsim = 10000, vecarea = rep(10, times = 10000))
+save(sim_Avar10, file = "data/sim_Avar10.Rdata")
+
+#simulacao mais simples, com area constante em 50
+# nsim foi colocado como 10000 para ver se o modelo misto para comparacao com simulacao anterior
+sim_Avar50 = saposim(nsim = 10000, vecarea = rep(50, times = 10000))
+save(sim_Avar, file = "data/sim_Avar50.Rdata")
+
 #simulacao mais simples, com area constante em 100
 # nsim foi colocado como 10000 para ver se o modelo misto para comparacao com simulacao anterior
-sim_Avar = saposim(nsim = 10000, vecarea = rep(100, times = 1000))
-save(sim_Avar, file = "data/sim_Avar.Rdata")
+sim_Avar10 = saposim(nsim = 10000, vecarea = rep(100, times = 10000))
+save(sim_Avar10, file = "data/sim_Avar10.Rdata")
