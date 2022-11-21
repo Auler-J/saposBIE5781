bba3c2d3e0e229f2cc142c587f08ec8d9b93d4ba
Auler-J <auler.jennifer@gmail.com>
2022-11-21 18:10:08 -0300

    arrumar ordem dos paineis AED

--- R/funcoes_auxiliares.R
+++ R/funcoes_auxiliares.R
@@ -41,44 +41,47 @@ logit2prob <- function(logit){
 
 #plots para analise exploratoria
 AED = function(dados, binw){
-  # 1. Histograma de tempo ate inicio da copula
-  hist <- ggplot(dados, aes(x=ttot)) +
-    geom_histogram(binwidth = binw, fill="#55a370", color = "darkslategrey", alpha=0.9) +
-    xlab("Tempo até início da cópula") +
-    theme_minimal()
-  # 2. Scatter Plots
+
+  # 1. Scatter Plots
   
-  ## 2.1 Tempo ate amplexo vs Prob. do macho ser escolhido
-  p1 = ggplot(dados, aes(prob, ttot, color=prob)) +
-    geom_point(shape=16, size = .2, show.legend = F, alpha = .5) +
-    theme_minimal() +
-    scale_color_gradient(low = "darkseagreen1", high = "darkslategrey") +
-    xlab("\nProb. do macho ser escolhido") +
-    ylab("Tempo até início da cópula\n")
-  
-  ## 2.2 Tempo ate amplexo vs taxa de foot-flagging
-  p2 = ggplot(dados, aes(foot, ttot, color=footpad)) +
-    geom_point(shape=16, size=.2, show.legend=F, alpha = .5) +
-    theme_minimal() +
-    scale_color_gradient(low = "darkseagreen1", high = "darkslategrey") +
-    xlab("\nTaxa de foot-flagging (n eventos/min)") +
-    ylab("Tempo até início da cópula\n")
-  
-  ## 2.3 Tempo ate amplexo vs DF
-  p3 = ggplot(dados, aes(freq, ttot, color=freqpad)) +
+  ## 1.1 Tempo ate amplexo vs DF
+  p1 = ggplot(dados, aes(freq, ttot, color=freqpad)) +
     geom_point(shape=16, size=.05, show.legend=F, alpha = .5) +
     theme_minimal() +
     scale_color_gradient(low = "darkseagreen1", high = "darkslategrey") +
     xlab("\nFrequência dominante (Hz)") +
     ylab("Tempo até início da cópula\n")
   
-  ## 2.4 Tempo ate amplexo vs SVL
-  p4 = ggplot(dados, aes(tama, ttot, color=tamapad)) +
+  ## 1.2 Tempo ate amplexo vs SVL
+  p2 = ggplot(dados, aes(tama, ttot, color=tamapad)) +
     geom_point(shape=16, size=.05, show.legend=F, alpha = .5) +
     theme_minimal() +
     scale_color_gradient(low = "darkseagreen1", high = "darkslategrey") +
     xlab("\nTamanho corporal (mm)") +
     ylab("Tempo até início da cópula\n")
   
-  grid.arrange(p1, p2, hist, p3, p4, nrow = 2)
+  ## 1.3 Tempo ate amplexo vs taxa de foot-flagging
+  p3 = ggplot(dados, aes(foot, ttot, color=footpad)) +
+    geom_point(shape=16, size=.2, show.legend=F, alpha = .5) +
+    theme_minimal() +
+    scale_color_gradient(low = "darkseagreen1", high = "darkslategrey") +
+    xlab("\nTaxa de foot-flagging (n eventos/min)") +
+    ylab("Tempo até início da cópula\n")
+  
+  ## 1.4 Tempo ate amplexo vs Prob. do macho ser escolhido
+  p4 = ggplot(dados, aes(prob, ttot, color=prob)) +
+    geom_point(shape=16, size = .2, show.legend = F, alpha = .5) +
+    theme_minimal() +
+    scale_color_gradient(low = "darkseagreen1", high = "darkslategrey") +
+    xlab("\nProb. do macho ser escolhido") +
+    ylab("Tempo até início da cópula\n")
+  
+  # 2. Histograma de tempo ate inicio da copula
+  hist <- ggplot(dados, aes(x=ttot)) +
+    geom_histogram(binwidth = binw, fill="#55a370", color = "darkslategrey", alpha=0.9) +
+    xlab("\nTempo até início da cópula") +
+    ylab("Número de machos\n") +
+    theme_minimal()
+  
+  grid.arrange(p1, p2, p3, p4, hist, nrow = 2)
 }
