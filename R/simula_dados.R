6256625dc65170546ecc8257ced10fb002016774
Auler-J <auler.jennifer@gmail.com>
2022-11-21 16:34:38 -0300

    mudamos parametros foot e tama da simulacao e nsim para 1000

--- R/simula_dados.R
+++ R/simula_dados.R
@@ -1,22 +1,22 @@
 source("R/saposim.R")
 source("R/funcoes_auxiliares.R")
 
-# simulacao com valores default: vecarea = round(runif(nsim, 10, 100), 0), vecnmacho= vecarea, cffreq = 1.05, cftama = 0.5, cffoot = 0.8
-# nsim foi colocado como 10000 para ver se o modelo misto converge
-sim_Avar = saposim(nsim = 10000)
+#valores defaut:
+#vecnmacho= vecarea, cffreq = 1.05, cftama = 0.5, cffoot = 0.8, nsim = 1000
+
+# simulacao com vecarea default: vecarea = round(runif(nsim, 10, 100), 0)
+sim_Avar = saposim()
 save(sim_Avar, file = "data/sim_Avar.Rdata")
 
 #simulacao mais simples, com area constante em 10
-# nsim foi colocado como 10000 para ver se o modelo misto para comparacao com simulacao anterior
-sim_Afix10 = saposim(nsim = 10000, vecarea = rep(10, times = 10000))
+sim_Afix10 = saposim(vecarea = rep(10, times = 1000))
 save(sim_Afix10, file = "data/sim_Afix10.Rdata")
 
 #simulacao mais simples, com area constante em 50
-# nsim foi colocado como 10000 para ver se o modelo misto para comparacao com simulacao anterior
-sim_Afix50 = saposim(nsim = 10000, vecarea = rep(50, times = 10000))
+
+sim_Afix50 = saposim(vecarea = rep(50, times = 1000))
 save(sim_Afix50, file = "data/sim_Afix50.Rdata")
 
 #simulacao mais simples, com area constante em 100
-# nsim foi colocado como 10000 para ver se o modelo misto para comparacao com simulacao anterior
-sim_Afix100 = saposim(nsim = 10000, vecarea = rep(100, times = 10000))
+sim_Afix100 = saposim(vecarea = rep(100, times = 1000))
 save(sim_Afix100, file = "data/sim_Afix100.Rdata")
